f7bbae33961032a32fcd5ce72b5e20c6a4471a53
grakk <liboming88@yeah.net>
2022-07-14 17:15:17 +0800

    add:tailwindcss

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>tailwindcss</title>
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+</body>
+</html>
